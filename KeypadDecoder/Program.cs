aee68ba757abe5df4167e5887994f88191d5e513
Sherajus Salehin <anmssalehin@gmail.com>
2025-10-25 04:26:27 +0600

    Reimplemented main function

--- KeypadDecoder/Program.cs
+++ KeypadDecoder/Program.cs
@@ -2,19 +2,17 @@
 {
     static void Main(string[] args)
     {
-        var inputs = new List<string>{
-            "436**22243#",
-            "33#",
-            "227*#",
-            "4433555 555666#",
-            "8 88777444666*664#",
-            "12"
-        };
-        foreach (var input in inputs)
-        {
-            Console.WriteLine(input);
-            var decodedStr = KeypadDecoder.Solution.OldPhonePad(input);
-            Console.WriteLine($">> {decodedStr}");
+        Console.WriteLine("Enter \'q\' to quit");
+
+        while (true) {
+            Console.Write("Enter Sequence: ");
+            var input = Console.ReadLine();
+            if (input == null) continue;
+
+            input = input.Trim();
+            if (input == "q") break;
+            if (input.Length > 0 && input[input.Length - 1] != '#') input += "#";
+            Console.WriteLine($"Decoded text: {KeypadDecoder.Solution.OldPhonePad(input)}");
         }
     }
 }
